7496fb2b21ff4a9f587f2b46c6cdd2191c66e19d
Frykte <in_wakahana@outlook.com>
2023-10-12 02:38:33 +0900

    3

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Git!");
+		System.out.println("update1");
+		System.out.println("update2");
+		System.out.println("update3");
+		
+	}
+
+}
